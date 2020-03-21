d15ba6c6bd80665d363649a35fcc7633fa7778f9
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-21 23:48:18 +0530

    Added search function

--- article.py
+++ article.py
@@ -4,25 +4,29 @@ import requests
 import nltk
 #nltk.download("punkt")
 
+def search(soup):
+    results = soup.find_all(text="Union",recursive=True)
+    print(len(results))
+
 web_url = "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen"
 
 r = requests.get(web_url)
 soup = BeautifulSoup(r.text,'html.parser')
-link_set = soup.find_all('a', class_ = "DY5T1d")
-count = 5
 base = "https://news.google.com/"
+count = 5
 
 for i in soup.find_all('a', class_ = "DY5T1d"):
     if(count==0):
         break
     str = base + i['href']
-    darticle = Article(str)
+    article = Article(str)
     
-    darticle.download()
-    darticle.parse()
-    darticle.nlp()
+    article.download()
+    article.parse()
+    article.nlp()
     
-    print("\nTitle:" + darticle.title)
-    print("\nSummary:" + darticle.summary)
+    print("\nTitle:" + article.title)
+    print("Summary:" + article.summary)
     count = count - 1
 
+search(soup)
